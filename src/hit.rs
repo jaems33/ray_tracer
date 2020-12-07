51e09e56f2f7f16b8281535fa1b15c58762ad0fd
James Teow <james.teow83@gmail.com>
2020-12-06 19:45:06 -0500

    Add Hittable trait

---
+++ src/hit.rs
@@ -0,0 +1,12 @@
+use super::lib::{Vec3, Point3};
+use super::ray::{Ray};
+
+pub struct HitRecord {
+  p: Point3,
+  normal: Vec3,
+  t: f64
+}
+
+trait Hittable {
+  fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &HitRecord) -> bool;
+}
